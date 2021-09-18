8de35e4425b00886116036d06aad1f81534ee97f
mafei <mafei.dev@gmail.com>
2021-09-18 23:10:01 +0530

    SERVICE_HEALTH_DATA transferred to common

--- src/main/java/com/mafei/stacksaga/common/resources/StaticData.java
+++ src/main/java/com/mafei/stacksaga/common/resources/StaticData.java
@@ -1,9 +1,16 @@
 package com.mafei.stacksaga.common.resources;
 
+import com.mafei.stacksaga.common.beans.health.ServiceHealth;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class StaticData {
 
     /**
      * instance topic = [INSTANCE_TOPIC_PREFIX+server name + instance id]
      */
     public static final String INSTANCE_TOPIC_PREFIX = "INSTANCE_";
+    public final static Map<String, ServiceHealth> SERVICE_HEALTH_DATA = new HashMap<>();
+
 }
