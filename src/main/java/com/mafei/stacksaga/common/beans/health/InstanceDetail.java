80ca77f877afff4b631e99465f06a0855debd31d
mafei <mafei.dev@gmail.com>
2021-09-24 03:21:17 +0530

    NEW_SERVER_UP_TOPIC event configured successfully.

--- src/main/java/com/mafei/stacksaga/common/beans/health/InstanceDetail.java
+++ src/main/java/com/mafei/stacksaga/common/beans/health/InstanceDetail.java
@@ -1,21 +1,25 @@
 package com.mafei.stacksaga.common.beans.health;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.io.Serializable;
 
 @Data
 @ToString
-@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.USE_DEFAULTS)
 public class InstanceDetail implements Serializable {
+    private static final long serialVersionUID = 14L;
     private String instance_id;
-    private String instance_name;
     private String service_name;
-    private String last_updated_datetime;
+    private Long start_up_datetime;
+    private Long up_time;
+    private Long last_updated_datetime;
     private String status;
     private String lats_server_up_datetime;
     // TODO: 9/18/2021 add config data each server from property file
 
+    public InstanceDetail() {
+    }
 }
